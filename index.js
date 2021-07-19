a3700b95ebd1ce33c89dd77456c2d99d7c2ea751
nj10deadend <nasjacks10@gmail.com>
2021-07-19 11:06:45 -0400

    commit tree a3ce4b2b4c6e98ab258e2905ea23ae05cafad789

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = `Grace Hopper`; // Write your code in this file!
+const welcomeMessage = `Welcome to Flatbook, ` + currentUser + `!`;
+//const excitedWelcomeMessage = `WELCOME TO FLATBOX, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0 , 1)}!`;
